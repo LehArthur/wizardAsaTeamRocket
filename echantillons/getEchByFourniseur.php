1371e7ae3e86e4ce340689d3c4c61e06d9d81a4f
System Administrator <root@MinideFrederic.home>
2016-07-11 16:06:29 +0200

    few chanegs

--- echantillons/getEchByFourniseur.php
+++ echantillons/getEchByFourniseur.php
@@ -20,7 +20,7 @@ $query = $SELECT.$FROM.$WHERE;
 $query .= ") ORDER BY FOURNISSEUR DESC";
 $message .= $query . "\n";
 
-$list = "";
+$list = array();
 
 if ($conn = connectDB()) {
 	if ($rs = $conn->query($query)) {
@@ -32,37 +32,11 @@ if ($conn = connectDB()) {
 		while ($row = $rs->fetch_assoc()) {
 			$idEchantillon = $row['idEchantillon'];
 			$list[$idEchantillon] = $row;
-			$list[$idEchantillon]['fournisseur'];
-
-			//Search associated files
-			$query2 = "SELECT id, type, name, size, url, deleteUrl, deleteType FROM files f JOIN files_attached fa ON f.id = fa.idFile WHERE fa.idData = '$idContract' AND fa.typeData = 'contract'";
-			$message .= $query2 . "\n";
-			if ($rs2 = $conn->query($query2)) {
-				$rows_returned = $rs2->num_rows;
-				$message .= "[OK] $rows_returned files.\n";
-
-				$rs2->data_seek(0);
-				$files = array();
-				while ($file = $rs2->fetch_assoc()) {
-					$files[] = $file;
-				}
-			}
-			$list[$idContract]['files'] = $files;
-			$rs2->close();
 		}
 		$rs->close();
 	}
 	$conn->close();
-} /*
-else {
-
-}*/
-
-if(getenv("debug")){
-	$out['log'] = $message;
 }
+
 echo json_encode($list);
-
 ?>
-
-
---
+++ echantillons/getEchByInstrument.php
@@ -0,0 +1,42 @@
+<?php
+
+//include '../include/util.php';
+include 'connectDB.php';
+
+
+//{mainLoc: mainLoc, subLoc: subLoc, dateFrom: dateFrom, dateTo: dateTo, author: author, status: status, category: category}
+$message = "ajax call \n";
+
+$fournisseur = isset($_REQUEST["fournisseur"]) ? $_REQUEST["fournisseur"] : "%";
+$message .= "fournisseur: ".$fournisseur."\n";
+
+//Request to DB
+$SELECT = "SELECT * ";
+$FROM = "FROM echantillon";
+$WHERE = "WHERE FOURNISSEUR = '$fournisseur'";
+
+$query = $SELECT.$FROM.$WHERE;
+
+$query .= ") ORDER BY FOURNISSEUR DESC";
+$message .= $query . "\n";
+
+$list = array();
+
+if ($conn = connectDB()) {
+	if ($rs = $conn->query($query)) {
+		$rows_returned = $rs->num_rows;
+		$message .= "[OK] $rows_returned results.\n";
+		//Iterate over the recordset
+
+		$rs->data_seek(0);
+		while ($row = $rs->fetch_assoc()) {
+			$idEchantillon = $row['idEchantillon'];
+			$list[$idEchantillon] = $row;
+		}
+		$rs->close();
+	}
+	$conn->close();
+}
+
+echo json_encode($list);
+?>
